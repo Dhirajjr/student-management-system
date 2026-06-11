92d571c4a864e01b2cdaec5d1f38738d1bcdce5c
Dhiraj <dhirajrangar15@gmail.com>
2026-06-11 16:22:44 +0530

    Delete student feature

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Student Management System");
         System.out.println("Add Student Feature");
+        System.out.println("Delete Student Feature");
     }
 }
